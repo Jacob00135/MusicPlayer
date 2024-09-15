bbed9f3b4eeb0e92bfdccca22ba8ae76997ccd7f
Jacob00135 <2428207444@qq.com>
2024-09-15 08:59:15 +0800

    完成功能：上一首、下一首、播放模式

--- static/js/common.js
+++ static/js/common.js
@@ -1,3 +1,29 @@
+function generateRangeArray(min, max) {
+    const array = [];
+    for (let i = min; i < max; i++) {
+        array.push(i);
+    }
+    return array;
+}
+
+function inArray(value, array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    if (min > max) {
+        return NaN;
+    }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function ajaxGetJson(url, callback) {
     const xhr = new XMLHttpRequest();
     xhr.open('get', url, true);
--- static/js/player.js
+++ static/js/player.js
@@ -2,6 +2,8 @@
     'use strict';
 
     const mainElement = document.getElementById('main');
+    const playModeArray = ['sequential', 'list-loop', 'one-loop', 'random'];
+    let sourcePlayList;
     let playlist;
 
     mainElement.querySelector('.progress-bar .background').addEventListener('click', locatePlayProgress);
@@ -10,7 +12,127 @@
     document.getElementById('fast-backward-btn').addEventListener('click', fastBackwardButtonClickEvent);
     document.getElementById('fast-forward-btn').addEventListener('click', fastForwardButtonClickEvent);
     document.getElementById('play-mode').addEventListener('click', playModeButtonClickEvent);
-    loadCurrentPlaylist()
+    document.getElementById('prev-song-btn').addEventListener('click', prevSongButtonClickEvent);
+    document.getElementById('next-song-btn').addEventListener('click', nextSongButtonClickEvent);
+    loadCurrentPlaylist();
+
+    class Playlist {
+        #getPrevSongFunction = null;
+        #getNextSongFunction = null;
+
+        constructor(array, playMode) {
+            this.array = array;
+            this.playMode = playMode;
+            this.currentIndex = 0;
+            this.randomIndexArray = generateRangeArray(0, this.array.length);
+
+            this.#allocateFunction();
+        }
+
+        getCurrentSong() {
+            return this.array[this.randomIndexArray[this.currentIndex]];
+        }
+
+        getPrevSong() {
+            return this.#getPrevSongFunction();
+        }
+
+        getNextSong() {
+            return this.#getNextSongFunction();
+        }
+
+        #allocateFunction() {
+            if (this.playMode === 'sequential') {
+                this.#getPrevSongFunction = this.#getSequentialModePrevSong;
+                this.#getNextSongFunction = this.#getSequentialModeNextSong;
+            } else if (this.playMode === 'list-loop') {
+                this.#getPrevSongFunction = this.#getListloopModePrevSong;
+                this.#getNextSongFunction = this.#getListloopModeNextSong;
+            } else if (this.playMode === 'one-loop') {
+                this.#getPrevSongFunction = this.#getOneLoopModePrevSong;
+                this.#getNextSongFunction = this.#getOneLoopModeNextSong;
+            } else if (this.playMode === 'random') {
+                this.#shuffleRandomIndexArray();
+                this.#getPrevSongFunction = this.#getRandomModePrevSong;
+                this.#getNextSongFunction = this.#getRandomModeNextSong;
+            }
+        }
+
+        #shuffleRandomIndexArray() {
+            this.randomIndexArray = generateRangeArray(0, this.array.length);
+
+            const maxIndex = this.randomIndexArray.length - 1;
+            const shuffleCount = this.randomIndexArray.length * 10;
+            for (let i = 0; i < shuffleCount; i++) {
+                let j = getRandomInt(0, maxIndex);
+                let k = getRandomInt(0, maxIndex);
+                let tmp = this.randomIndexArray[j];
+                this.randomIndexArray[j] = this.randomIndexArray[k];
+                this.randomIndexArray[k] = tmp;
+            }
+        }
+
+        #getSequentialModePrevSong() {
+            if (this.currentIndex === 0) {
+                return null;
+            }
+
+            this.currentIndex = this.currentIndex - 1;
+            return this.array[this.randomIndexArray[this.currentIndex]];
+        }
+
+        #getSequentialModeNextSong() {
+            if (this.currentIndex === this.array.length - 1) {
+                return null;
+            }
+
+            this.currentIndex = this.currentIndex + 1;
+            return this.array[this.randomIndexArray[this.currentIndex]];
+        }
+
+        #getListloopModePrevSong() {
+            if (this.currentIndex === 0) {
+                this.currentIndex = this.array.length - 1;
+            } else {
+                this.currentIndex = this.currentIndex - 1;
+            }
+
+            return this.array[this.randomIndexArray[this.currentIndex]];
+        }
+
+        #getListloopModeNextSong() {
+            if (this.currentIndex === this.array.length - 1) {
+                this.currentIndex = 0;
+            } else {
+                this.currentIndex = this.currentIndex + 1;
+            }
+
+            return this.array[this.randomIndexArray[this.currentIndex]];
+        }
+
+        #getOneLoopModePrevSong() {
+            return this.#getListloopModePrevSong();
+        }
+
+        #getOneLoopModeNextSong() {
+            return this.#getListloopModeNextSong();
+        }
+
+        #getRandomModePrevSong() {
+            return this.#getSequentialModePrevSong();
+        }
+
+        #getRandomModeNextSong() {
+            if (this.currentIndex === this.array.length - 1) {
+                this.#shuffleRandomIndexArray();
+                this.currentIndex = 0;
+            } else {
+                this.currentIndex = this.currentIndex + 1;
+            }
+
+            return this.array[this.randomIndexArray[this.currentIndex]];
+        }
+    }
 
     function loadCurrentPlaylist() {
         const url = mainElement.getAttribute('data-get-current-playlist-route');
@@ -20,9 +142,10 @@
                 return undefined;
             }
 
-            playlist = responseData.data;
-            if (playlist.length !== 0) {
-                const audioElement = generateAudioElement(playlist[0]);
+            if (responseData.data.length !== 0) {
+                sourcePlayList = responseData.data;
+                playlist = new Playlist(responseData.data, 'sequential');
+                const audioElement = generateAudioElement(playlist.getCurrentSong());
                 insertAudioElement(audioElement);
             }
         });
@@ -110,10 +233,17 @@
     }
 
     function audioEndedEvent(e) {
+        const currentPlayMode = document.getElementById('play-mode').getAttribute('data-mode');
+        if (currentPlayMode === 'one-loop') {
+            mainElement.querySelector('audio').play();
+            return undefined;
+        }
+
         mainElement.querySelector('.progress-bar .current-time').innerHTML = transformTime(e.target.duration);
         document.getElementById('icon-pause').classList.add('hidden');
         document.getElementById('icon-play').classList.remove('hidden');
         document.getElementById('play-btn').setAttribute('data-state', 'pause');
+        document.getElementById('next-song-btn').click();
     }
 
     function locatePlayProgress(e) {
@@ -162,19 +292,60 @@
         }
     }
 
+    function switchSong(filepath) {
+        mainElement.querySelector('audio').remove();
+        const audioElement = generateAudioElement(filepath);
+        insertAudioElement(audioElement);
+
+        audioElement.play();
+        document.getElementById('icon-play').classList.add('hidden');
+        document.getElementById('icon-pause').classList.remove('hidden');
+        document.getElementById('play-btn').setAttribute('data-state', 'play');
+    }
+
     function playModeButtonClickEvent(e) {
         const btn = document.getElementById('play-mode');
-        const iconArr = btn.children;
-        const currentPlayMode = btn.getAttribute('data-mode');
-        for (let i = 0; i < iconArr.length; i++) {
-            if (iconArr[i].getAttribute('data-mode') === currentPlayMode) {
-                const newIndex = (i + 1) % iconArr.length;
-                iconArr[i].classList.add('hidden');
-                iconArr[newIndex].classList.remove('hidden');
-                btn.setAttribute('title', iconArr[newIndex].getAttribute('data-title'));
-                btn.setAttribute('data-mode', iconArr[newIndex].getAttribute('data-mode'));
-                break;
-            }
+        const oldPlayMode = btn.getAttribute('data-mode');
+        let i = 0;
+        while (i < playModeArray.length && (playModeArray[i] !== oldPlayMode)) {
+            i = i + 1;
         }
+
+        const newPlayMode = playModeArray[(i + 1) % playModeArray.length];
+        btn.children[i].classList.add('hidden');
+        btn.children[(i + 1) % playModeArray.length].classList.remove('hidden');
+        btn.setAttribute('title', newPlayMode);
+        btn.setAttribute('data-mode', newPlayMode);
+
+        if (inArray(oldPlayMode, ['sequential', 'list-loop', 'one-loop']) && newPlayMode === 'random') {
+            playlist = new Playlist(sourcePlayList, 'random');
+            switchSong(playlist.getCurrentSong());
+        } else if (oldPlayMode === 'random' && (newPlayMode === 'sequential' || newPlayMode === 'list-loop')) {
+            const randomIndex = playlist.randomIndexArray[playlist.currentIndex];
+            playlist = new Playlist(sourcePlayList, newPlayMode);
+            playlist.currentIndex = randomIndex;
+        } else {
+            const currentIndex = playlist.currentIndex;
+            playlist = new Playlist(sourcePlayList, newPlayMode);
+            playlist.currentIndex = currentIndex;
+        }
+    }
+
+    function prevSongButtonClickEvent(e) {
+        const filepath = playlist.getPrevSong();
+        if (filepath === null) {
+            return undefined;
+        }
+
+        switchSong(filepath);
+    }
+
+    function nextSongButtonClickEvent(e) {
+        const filepath = playlist.getNextSong();
+        if (filepath === null) {
+            return undefined;
+        }
+
+        switchSong(filepath);
     }
 })();
